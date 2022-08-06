87e2b7d35c316a2ecc82ca1a2259432e911b75f6
Théo Mesnil <mesniltheo@gmail.com>
2022-08-06 17:28:09 +0200

    chore: upgrade expo and librairies
    Signed-off-by: Théo Mesnil <mesniltheo@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'intl';
 import 'intl/locale-data/jsonp/en';
 import 'intl/locale-data/jsonp/fr';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import {
   Poppins_400Regular,
   Poppins_600SemiBold,
   useFonts
 } from '@expo-google-fonts/poppins';
-import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { Box } from 'components/Box';
 import { coreTheme } from 'themes/core';
@@ -18,33 +18,41 @@ import { IntlMessages } from 'components/IntlMessages';
 import { GenresProvider } from 'contexts/genres';
 import { Navigation } from 'navigation';
 
+SplashScreen.preventAutoHideAsync();
+
 export function AppIndex() {
   let [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_600SemiBold
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <ThemeProvider theme={coreTheme}>
-        <StatusBar
-          barStyle="light-content"
-          animated
-          backgroundColor="transparent"
-          translucent
-        />
-        <LocaleProvider>
-          <IntlMessages>
-            <GenresProvider>
-              <Box backgroundColor="behind" flex={1}>
-                <Navigation />
-              </Box>
-            </GenresProvider>
-          </IntlMessages>
-        </LocaleProvider>
-      </ThemeProvider>
-    );
+    return null;
   }
+
+  return (
+    <ThemeProvider theme={coreTheme}>
+      <StatusBar
+        barStyle="light-content"
+        animated
+        backgroundColor="transparent"
+        translucent
+      />
+      <LocaleProvider>
+        <IntlMessages>
+          <GenresProvider>
+            <Box backgroundColor="behind" flex={1} onLayout={onLayoutRootView}>
+              <Navigation />
+            </Box>
+          </GenresProvider>
+        </IntlMessages>
+      </LocaleProvider>
+    </ThemeProvider>
+  );
 }
