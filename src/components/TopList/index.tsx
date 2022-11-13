c37ee892fbd9d75b71f63dada2160e6b4c9347df
Théo Mesnil <mesniltheo@gmail.com>
2022-11-13 19:42:32 +0100

    fix: top 10 list modal
    Signed-off-by: Théo Mesnil <mesniltheo@gmail.com>

--- src/components/TopList/index.tsx
+++ src/components/TopList/index.tsx
@@ -65,7 +65,7 @@ export const TopList = React.memo(
         onPress({
           id: item?.id,
           type: item?.type || item?.media_type,
-          name: item?.name,
+          name: item?.name || item?.title,
           index
         });
     }
