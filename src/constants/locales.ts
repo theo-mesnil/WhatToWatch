ffdda39669d6c8dc1272392684a21f0efeefb092
Théo Mesnil <50322149+theo-mesnil@users.noreply.github.com>
2024-08-05 17:44:58 +0200

    feat: v3.1.1 (#105)
    * fix: wrong locale for EN
    
    * fix: eas
    
    * chore: upgrade version

--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -1,9 +1,10 @@
 import { getLocales } from 'expo-localization';
 
+export type Locale = 'en' | 'fr';
+export type LocaleI18n = 'en-US' | 'fr-FR';
+
 const LANGUAGE_CODE = getLocales()[0].languageCode;
 export const REGION_CODE = getLocales()[0].regionCode;
-export const LOCALE = LANGUAGE_CODE === 'fr' ? 'fr' : 'en';
-export const LOCALE_I18N = LANGUAGE_CODE === 'fr' ? 'fr-FR' : 'fr-FR';
-
-export type Locale = 'en' | 'fr';
-export type LocaleI18n = 'fr-FR' | 'fr-FR';
+export const LOCALE: Locale = LANGUAGE_CODE === 'fr' ? 'fr' : 'en';
+export const LOCALE_I18N: LocaleI18n =
+  LANGUAGE_CODE === 'fr' ? 'fr-FR' : 'en-US';
