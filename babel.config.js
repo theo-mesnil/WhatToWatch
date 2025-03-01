adc9dd11b413fc8ef5cc82c95d72d5193d8b5da3
Théo Mesnil <mesniltheo@gmail.com>
2025-03-01 22:33:21 +0100

    fix: linters

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = function (api) {
           root: ['./src'],
           extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json']
         }
-      ],
+      ]
     ]
   };
 };
