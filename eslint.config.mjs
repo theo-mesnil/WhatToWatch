4f88047c5e06a575defddfa17b25b621305eb6e1
Théo Mesnil <mesniltheo@gmail.com>
2025-04-22 11:54:54 +0200

    chore: upgrade linters

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import eslint from '@eslint/js'
+import { config, configs } from 'typescript-eslint'
+import expolint from 'eslint-config-expo/flat.js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import { globalIgnores } from 'eslint/config'
+
+export default config(
+  eslint.configs.recommended,
+  configs.recommended,
+  expolint,
+  eslintPluginPrettierRecommended,
+  globalIgnores([
+    '.expo/*',
+    'dist/*',
+    'android/*',
+    'ios/*',
+    'web-build/*',
+    'node_modules/*',
+    'coverage/*',
+  ])
+)
